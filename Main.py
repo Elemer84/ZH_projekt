64017e4555fd514aae0772e3fadcc22b51493686
Elemer84 <holnapmatektz@gmail.com>
2024-11-26 12:01:43 +0100

    1

---
+++ Main.py
@@ -0,0 +1,9 @@
+# Kérje be a két számot a felhasználótól
+szam1 = float(input("Kérem, adja meg az első számot: "))
+szam2 = float(input("Kérem, adja meg a második számot: "))
+
+# Számolja ki az összeget
+osszeg = szam1 + szam2
+
+# Eredmény megjelenítése
+print(f"A két szám összege: {osszeg}")
